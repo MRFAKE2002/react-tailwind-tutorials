8a3ec004bed1ade1aad1035a5d39f56ca6b7b5b2
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2024-10-13 19:40:39 +0330

    Some codes tailwind tutorials

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,140 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* 
+        <h1
+          className="
+        text-center 
+        text-5xl 
+        font-bold 
+        text-red-700 
+        bg-black py-5"
+        >
+          Tailwind + React + Vite
+        </h1>
+        <div className="container">
+          <h3
+            className="
+          text-sky-400 
+          text-4xl 
+          font-black 
+          text-center 
+          my-4 
+          shadow-lg 
+          shadow-blue-600 
+          bg-yellow-300 
+          rounded-xl"
+          >
+            test
+          </h3>
+        </div>
+      
+        <h2 className="inline-block bg-slate-700 h-9">test2</h2>
+        <h1 className="overflow-auto text-center w-10 bg-orange-400">
+          overflow test
+        </h1>
+        <div className="bg-black h-40 relative mt-10">
+          <h1 className="bg-red-700 size-20 absolute right-0">test4</h1>
+          <h1 className="bg-yellow-400 size-20 fixed opacity-80 rotate-45 z-20">
+            test5
+          </h1>
+          <h1 className="bg-blue-500 size-20 fixed opacity-80 rotate-45 z-40">
+            test6
+          </h1>
+        </div>
+
+      */}
+
+      {/* flex */}
+
+      {/* 
+        <div className="bg-slate-800 h-96 flex justify-center items-center">
+          <h1 className="bg-yellow-500 size-20">flex test</h1>
+        </div> 
+
+        <div className="bg-slate-800 h-96 flex justify-between items-center">
+          <h1 className="bg-yellow-500 size-20">flex test</h1>
+          <h1 className="bg-yellow-500 size-20">flex test</h1>
+          <h1 className="bg-yellow-500 size-20">flex test</h1>
+          <h1 className="bg-yellow-500 size-20">flex test</h1>
+        </div>
+      
+        <div className="bg-slate-800 h-96 flex justify-between items-center flex-row">
+          <h1 className="bg-yellow-500 h-20 basis-1/4">flex test</h1>
+          <h1 className="bg-yellow-500 h-20 basis-2/4">flex test</h1>
+          <h1 className="bg-yellow-500 h-20 basis-1/4">flex test</h1>
+          <h1 className="bg-yellow-500 h-20 basis-3/4">flex test</h1>
+        </div>
+
+        <div className="bg-black h-96 flex justify-center items-center">
+          <h1 className="bg-red-700 text-2xl text-white flex justify-center items-center h-24 w-24">
+            flex test
+          </h1>
+        </div>
+      */}
+
+      {/* grid */}
+
+      {/* 
+        <div className="bg-black h-96 grid grid-cols-4 gap-3">
+          <h1 className="bg-red-700 text-2xl text-white h-24 col-span-2">flex test</h1>
+          <h1 className="bg-red-700 text-2xl text-white h-24 col-span-2">flex test</h1>
+          <h1 className="bg-red-700 text-2xl text-white h-24 col-span-3">flex test</h1>
+          <h1 className="bg-red-700 text-2xl text-white h-24 col-span-1">flex test</h1>
+        </div> 
+      
+        <div className="bg-black h-96 grid grid-cols-12 gap-3">
+          <h1 className="bg-red-700 text-2xl text-white h-24 col-span-12 sm:col-span-6 md:col-span-3 xl:col-span-4 2xl:text-7xl 2xl:col-span-2">flex test</h1>
+          <h1 className="bg-red-700 text-2xl text-white h-24 col-span-12 sm:col-span-6 md:col-span-3 xl:col-span-4 2xl:text-7xl 2xl:col-span-2">flex test</h1>
+          <h1 className="bg-red-700 text-2xl text-white h-24 col-span-12 sm:col-span-6 md:col-span-3 xl:col-span-4 2xl:text-7xl 2xl:col-span-2">flex test</h1>
+          <h1 className="bg-red-700 text-2xl text-white h-24 col-span-12 sm:col-span-6 md:col-span-3 xl:col-span-4 2xl:text-7xl 2xl:col-span-2">flex test</h1>
+          <h1 className="bg-red-700 text-2xl text-white h-24 col-span-12 sm:col-span-6 md:col-span-3 xl:col-span-4 2xl:text-7xl 2xl:col-span-2">flex test</h1>
+          <h1 className="bg-red-700 text-2xl text-white h-24 col-span-12 sm:col-span-6 md:col-span-3 xl:col-span-4 2xl:text-7xl 2xl:col-span-2">flex test</h1>
+        </div> 
+      */}
+
+      
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </>
   );
 }
